73e0e4219af453093775463eb3a1857670837690
smnrkzl <kizilsemanur@gmail.com>
2024-08-18 18:06:34 +0300

    Add project files.

---
+++ ECommerce.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities
+{
+    public  class Product:Entity
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string BrandId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
